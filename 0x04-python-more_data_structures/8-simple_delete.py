19de4773ca313a49971ca21dc7d656c5ff5b3b9c
Thlakudi <thlakudimsibi760@gmail.com>
2024-01-11 01:59:03 +0000

    sim delete

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-	my_dict.pop(key, None)
-	return my_dict
+def simple_delete(my_dict, key=""):
+    my_dict.pop(key, None)
+    return my_dict
