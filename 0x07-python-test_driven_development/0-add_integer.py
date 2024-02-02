05b859d3e4a986a91cd8dba234d41d54db52ebfd
Thlakudi <thlakudimsibi760@gmail.com>
2024-02-01 21:50:58 -0800

    second try addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,15 +2,20 @@
 def add_integer(a, b = 98):
     """this function adds two int or float together"""
     if not isinstance(a, (int, float)):
+        """checks if a is int or float"""
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
+        """checks if b is int or float"""
         raise TypeError("b must be an integer")
 
     a = int(a)
+    """converts a to int if not"""
     b = int(b)
+    """converts b to int if not"""
 
     return (a + b)
 
 if __name__ == "__main__":
+    """documentation included in att files and folders"""
     print(__import__("my_module").__doc__)
     print(__import__("my_module").add_integer.__doc__)
