f062b5bd1db09f78669f833a7b29e4a28c05a314
Thlakudi <thlakudimsibi760@gmail.com>
2024-02-01 21:47:12 -0800

    second try addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def add_integer(a, b = 98):
+    """this function adds two int or float together"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
