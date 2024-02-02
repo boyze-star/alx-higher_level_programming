e40955b4019c774e10c89c725cc213a4dae5ec32
Thlakudi <thlakudimsibi760@gmail.com>
2024-02-01 21:43:17 -0800

    second try addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,26 +1,5 @@
 #!/usr/bin/python3
-
 def add_integer(a, b = 98):
-    """
-    Adds two integers.
-
-    :param a: The first integer.
-    :param b: The second integer (default is 98).
-    :return: The sum of a and b as an integer.
-    :raises TypeError: If a or b is not an integer or float.
-
-    Examples:
-    >>> add_integer(5, 3)
-    8
-
-    >>> add_integer(2.5, 3)
-    5
-
-    >>> add_integer("a", 3)
-    Traceback (most recent call last):
-        ...
-    TypeError: a must be an integer or b must be an integer
-    """
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer or b must be an integer")
     if not isinstance(b, (int, float)):
